0eb72f6fa7548c8340aa0c39073a2e270422a09d
yusufcodes <yusufcodes@outlook.com>
2019-08-09 10:02:36 +0100

    Added Post.php - Model

---
+++ app/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App;
+
+class Post
+{
+    public function getPosts($session)
+    {
+        if (!session->has('posts'))
+        {
+            $this->createDummyData($session);
+        }
+        return $session->get('posts');
+    }
+
+    private function createDummyData($session)
+    {
+        $posts = [
+            [
+                'title' => 'Hello Laravel',
+                'content' => 'This is one of the blog posts...'
+            ],
+
+            [
+                'title' => 'Laravel Post Two',
+                'content' => 'This is another blog post...'
+            ]
+
+        ];
+
+        $session->put('posts', $posts);
+    }
+}
