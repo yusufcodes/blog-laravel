41008b5e0aa33055ce4219f4feccd1f8815981b9
yusufcodes <yusufcodes@outlook.com>
2019-08-09 10:14:12 +0100

    Added method to PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
+
+// Sessions
+use Illuminate\Session\Store;
+
+// use App\Http\Requests;
 
 class PostController extends Controller
 {
-    //
+    public function getIndex(Store $session)
+    {
+        $post = new Post();
+        $posts = $post->getPosts($session);
+
+        return view('blog.index', ['posts' => $posts])
+    }
 }
