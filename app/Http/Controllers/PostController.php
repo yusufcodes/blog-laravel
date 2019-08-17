7d3a4e37587b8d85330757ee0d4d874db27184bf
yusufcodes <yusufcodes@outlook.com>
2019-08-17 16:25:49 +0100

    Edited postAdminUpdate to update posts stored in the database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,22 +12,22 @@ use App\Http\Requests;
 
 class PostController extends Controller
 {
-    public function getIndex(Store $session)
+    public function getIndex()
     {
         $posts = Post::all();
         return view('blog.index', ['posts' => $posts]);
     }
 
-    public function getAdminIndex(Store $session)
+    public function getAdminIndex()
     {
         $posts = Post::all();
         return view('admin.index', ['posts' => $posts]);
     }
 
-    public function getPost(Store $session, $id)
+    public function getPost($id)
     {
         $post = Post::find($id);
-        return view('blog.post', ['post' => $posts]);
+        return view('blog.post', ['post' => $post]);
     }
 
     public function getAdminCreate()
@@ -35,12 +35,10 @@ class PostController extends Controller
         return view('admin.create');
     }
 
-    public function getAdminEdit(Store $session, $id)
+    public function getAdminEdit($id)
     {
-        $post = new Post();
-        /*!!!*/
-        $posts = $post->getPost($session, $id);
-        return view('admin.edit', ['post' => $posts, 'postId' => $id]);
+        $post = Post::find($id);
+        return view('admin.edit', ['post' => $post, 'postId' => $id]);
     }
 
     public function postAdminCreate(Store $session, Request $request)
@@ -72,13 +70,18 @@ class PostController extends Controller
             'content' => 'required|min:10'
         ]);
 
-        $post = new Post();
-        $post->editPost(
-            $session,
-            $request->input('id'),
-            $request->input('title'),
-            $request->input('content')
-        );
+        // $post = new Post();
+        // $post->editPost(
+        //     $session,
+        //     $request->input('id'),
+        //     $request->input('title'),
+        //     $request->input('content')
+        // );
+
+        $post = Post::find($request->input('id'));
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
         
         return redirect()
         ->route('admin.index')
--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -8,11 +8,11 @@
             <form action="{{ route('admin.update') }}" method="post">
                 <div class="form-group">
                     <label for="title">Title</label>
-                    <input type="text" class="form-control" id="title" name="title" value="{{$post['title']}}">
+                    <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}">
                 </div>
                 <div class="form-group">
                     <label for="content">Content</label>
-                    <input type="text" class="form-control" id="content" name="content" value="{{$post['content']}}">
+                    <input type="text" class="form-control" id="content" name="content" value="{{$post->content}}">
                 </div>
                 {{ csrf_field() }}
 
